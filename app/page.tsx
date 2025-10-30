781516b45aeb07fa7a7003b00662f8ff6070b2fc
OneLastChanc3 <ytcarlos8045@gmail.com>
2025-10-30 12:23:07 -0500

    m}

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
+import ExploreBtn from "@/components/ExploreBtn";
 
 const page = () => {
   return (
-    <h1>
-        welcome to next.js 16
-    </h1>
-  )
-}
+    <section>
+      <h1 className="text-center">
+        The Hub For Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hacktons, Meetups and Conferences, All in one Place
+      </p>
+      <ExploreBtn/>
+      <div className="mt-20 space-y-7">
+        <h3>Featured Event</h3>
+        <ul className="events">
+            {[1,2,3,4,5].map((item) => 
+                <li key={item}>Event {item}</li>
+            )}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Image from "next/image";
+
+const ExploreBtn = () => {
+  return (
+    <button
+      onClick={() => console.log("clicked")}
+      type="button"
+      id="explore-btn"
+      className="mt-7 mx-auto"
+    >
+      <a href="#events">
+        Explore Events
+        <Image
+          src="public/icons/arrow-down.svg"
+          alt="arrow-down"
+          width={24}
+          height={24}
+        ></Image>
+      </a>
+    </button>
+  );
+};
+
+export default ExploreBtn;
